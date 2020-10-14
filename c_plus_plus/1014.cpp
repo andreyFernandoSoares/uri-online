e8c104540dc6c15d13a97096b9367f26e00cb142
Crixin <47997631+Crixin@users.noreply.github.com>
2020-10-14 08:27:36 -0300

    Create 1014.cpp

---
+++ c_plus_plus/1014.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+
+int x;
+double y;
+cout<<setprecision(1)<<fixed;
+cin >>x>>y;
+cout<<setprecision(3)<<fixed;
+cout<<x/y<<" km/l"<<endl;
+
+
+return 0;
+}
